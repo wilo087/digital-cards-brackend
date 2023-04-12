97e607a811a4c743224b2fce953ca3d026e7cd70
Wilowayne De La Cruz G <wilo0087@gmail.com>
2023-04-12 01:46:31 -0400

    Setting up the linter 😎

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import { ApolloServer } from '@apollo/server';
-import { startStandaloneServer } from '@apollo/server/standalone';
+import { ApolloServer } from '@apollo/server'
+import { startStandaloneServer } from '@apollo/server/standalone'
 
 // A schema is a collection of type definitions (hence "typeDefs")
 // that together define the "shape" of queries that are executed against
@@ -19,4 +19,16 @@ const typeDefs = `#graphql
   type Query {
     books: [Book]
   }
-`;
+`
+
+const books = [
+  {
+    title: 'The Awakening',
+    author: 'Kate Chopin'
+
+  },
+  {
+    title: 'City of Glass',
+    author: 'Paul Auster'
+  }
+]
